bff3c99bc0497d534d60568ae558b5df0044e561
lfgarcia <luis.garcia@provedatos.com>
2017-04-11 22:04:52 -0500

    - Estructura base del proyecto GameClub (Backend)

---
+++ gameclub/src/main/java/ec/com/levelap/gameclub/application/GameClubApplication.java
@@ -0,0 +1,83 @@
+package ec.com.levelap.gameclub.application;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import ec.com.levelap.base.LevelapBase;
+import ec.com.levelap.gameclub.utils.Const;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan({ Const.PACKAGE_NAMING })
+@EnableJpaRepositories({ Const.PACKAGE_NAMING })
+public class GameClubApplication extends GameClubConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameClubApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.getDefault());
+		return slr;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Bean
+	public LevelapBase levelapBase() {
+		return new LevelapBase(new GameClubBaseJpa());
+	}
+
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setDefaultEncoding(StandardCharsets.UTF_8.name());
+
+		return resolver;
+	}
+
+	@Bean
+	public ThreadPoolTaskScheduler taskScheduler() {
+		ThreadPoolTaskScheduler pool = new ThreadPoolTaskScheduler();
+		pool.setPoolSize(100);
+		return pool;
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+		restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		return restTemplate;
+	}
+
+}
---
+++ gameclub/src/main/java/ec/com/levelap/gameclub/application/GameClubConfiguration.java
@@ -0,0 +1,103 @@
+package ec.com.levelap.gameclub.application;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+import javax.mail.Session;
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.MultipartConfigElement;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jndi.JndiTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import ec.com.levelap.gameclub.utils.Const;
+
+@Configuration
+@EnableConfigurationProperties
+public class GameClubConfiguration extends SpringBootServletInitializer {
+
+	@Value("${spring.jpa.generate-ddl}")
+	private Boolean generateDll;
+
+	@Value("${spring.jpa.show-sql}")
+	private Boolean showSql;
+
+	@Bean
+	public DataSource dataBaseDataSource() throws NamingException {
+		JndiTemplate jndiTemplate = new JndiTemplate();
+		DataSource dataSource = (DataSource) jndiTemplate.lookup(Const.JNDI_DATA_BASE);
+		return dataSource;
+	}
+
+	@Bean
+	public JavaMailSender javaMailService() throws NamingException {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setDefaultEncoding(StandardCharsets.UTF_8.name());
+		mailSender.setSession(getMailSession());
+		return mailSender;
+	}
+
+	private Session getMailSession() throws NamingException {
+		JndiTemplate template = new JndiTemplate();
+		Session session = (Session) template.lookup(Const.JNDI_MAIL);
+		return session;
+	}
+
+	@Bean
+	public EntityManagerFactory entityManagerFactory() throws NamingException {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setGenerateDdl(this.generateDll);
+		vendorAdapter.setShowSql(this.showSql);
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		factory.setPackagesToScan(Const.PACKAGE_NAMING);
+		factory.setDataSource(this.dataBaseDataSource());
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.jdbc.batch_size", "100");
+		properties.setProperty("hibernate.order_inserts", "true");
+		properties.setProperty("hibernate.order_updates", "true");
+		factory.setJpaProperties(properties);
+		factory.afterPropertiesSet();
+		return factory.getObject();
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize("1024MB");
+		factory.setMaxRequestSize("1024MB");
+		return factory.createMultipartConfig();
+	}
+}

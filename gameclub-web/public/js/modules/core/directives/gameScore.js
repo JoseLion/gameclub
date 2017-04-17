56c30112f23d37d8890e1669ffe64e4a600acd0b
lfgarcia <luis.garcia@provedatos.com>
2017-04-17 15:09:58 -0500

    - Avance "Como Funciona"

--- gameclub-web/public/js/modules/core/directives/gameScore.js
+++ gameclub-web/public/js/modules/core/directives/gameScore.js
@@ -8,7 +8,6 @@ angular.module("Core").directive('gameScore', function() {
 		},
 		replace: true,
 		link: function($scope, element, attrs, ctrl) {
-			console.log($scope)
 			if($scope.score != null && !isNaN($scope.score)) {
 				for(let i=0 ; i<=$scope.score ; i++) {
 					angular.element('.score-'+i).attr('src', 'img/star-score.png')

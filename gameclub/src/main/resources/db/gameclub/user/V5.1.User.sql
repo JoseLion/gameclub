dadcaa7f7bd3bf02d17fbd3e1cf086f5a57b8e75
José Luis León <joseluis.provedatos@gmail.com>
2017-04-19 13:15:59 -0500

    - Usuarios del administrador

--- gameclub/src/main/resources/db/gameclub/user/V5.1.User.sql
+++ gameclub/src/main/resources/db/gameclub/user/V5.1.User.sql
@@ -1,2 +1,5 @@
-INSERT INTO gameclub.admin_user(full_name, username, password, profile)
-VALUES('José Luis León', 'jose.leon@provedatos.com', '$2a$12$CrGcBjTb/Yszy5LXuVksae5yPHpSI07QQM8ycknRQqIsAQtDXqpba', (SELECT id FROM gameclub.profile WHERE name='Administrador'));
+INSERT INTO gameclub.admin_user(full_name, username, password, profile) VALUES
+('José Luis León', 'jose.leon@provedatos.com', '$2a$12$CrGcBjTb/Yszy5LXuVksae5yPHpSI07QQM8ycknRQqIsAQtDXqpba', (SELECT id FROM gameclub.profile WHERE name='Administrador')),
+('Luis García', 'luis.garcia@provedatos.com', '$2a$12$CrGcBjTb/Yszy5LXuVksae5yPHpSI07QQM8ycknRQqIsAQtDXqpba', (SELECT id FROM gameclub.profile WHERE name='Administrador')),
+('Fernanda Lima', 'fernanda.lima@provedatos.com', '$2a$12$CrGcBjTb/Yszy5LXuVksae5yPHpSI07QQM8ycknRQqIsAQtDXqpba', (SELECT id FROM gameclub.profile WHERE name='Administrador')),
+('Christian Simpson', 'christian.simpson@levelapsoftware.com', '$2a$12$CrGcBjTb/Yszy5LXuVksae5yPHpSI07QQM8ycknRQqIsAQtDXqpba', (SELECT id FROM gameclub.profile WHERE name='Administrador'));

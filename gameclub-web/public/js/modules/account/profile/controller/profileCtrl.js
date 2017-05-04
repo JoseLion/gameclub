0cbb62bf4d09e9702d8ac5a23ce5b660ff2505cd
José Luis León <joseluis.provedatos@gmail.com>
2017-05-04 17:53:58 -0500

    - Avance

--- gameclub-web/public/js/modules/account/profile/controller/profileCtrl.js
+++ gameclub-web/public/js/modules/account/profile/controller/profileCtrl.js
@@ -1,8 +1,9 @@
-angular.module('Profile').controller('ProfileCtrl', function($scope, $rootScope, provinces, $state, getImageBase64, sweet, rest) {
+angular.module('Profile').controller('ProfileCtrl', function($scope, $rootScope, provinces, $state, getImageBase64, sweet, rest, getIndexOfArray) {
     $scope.contactInfo = {};
     $scope.contactMean = {fb:'Pablo Ponce'};
 
     $scope.file = {};
+    let tempUserInfo;
 
     provinces.$promise.then(function(data) {
         $scope.provinces = data;
@@ -12,6 +13,11 @@ angular.module('Profile').controller('ProfileCtrl', function($scope, $rootScope,
         setTimeout(function() {
             angular.element("#avatar_input").trigger('click');
         }, 0);
+    }
+
+    $scope.provinceRemoved = function() {
+        console.log("REMOVED");
+        $rootScope.currentUser.location = null;
     }
 
     $scope.save = function() {
@@ -29,6 +35,69 @@ angular.module('Profile').controller('ProfileCtrl', function($scope, $rootScope,
                 sweet.close();
             });
         });
+    }
+
+    $scope.getInfoPercentage = function() {
+        let percent = 0;
+        let factor = 100 / 13;
+
+        if ($rootScope.currentUser != null) {
+            if ($rootScope.currentUser.document != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.birthDate != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.profession != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.province != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.location != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.firstAddress != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.firstReceiver != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.firstMorningStartTime != null && $rootScope.currentUser.firstMorningEndTime != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.firstNoonStartTime != null && $rootScope.currentUser.firstNoonEndTime != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.secondAddress != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.secondReceiver != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.secondMorningStartTime != null && $rootScope.currentUser.secondMorningEndTime != null) {
+                percent += factor;
+            }
+
+            if ($rootScope.currentUser.secondNoonStartTime != null && $rootScope.currentUser.secondNoonEndTime != null) {
+                percent += factor;
+            }
+        }
+
+        percent = Math.round(percent);
+        $scope.InfoPercent = percent;
+        return percent;
     }
 
     $scope.$watch('file.avatar', function(newValue, oldValue) {
@@ -50,20 +119,34 @@ angular.module('Profile').controller('ProfileCtrl', function($scope, $rootScope,
         }
     });
 
-    $scope.isEditContact = false;
-    $scope.editContact = function() {
-        $scope.isEditContact = !$scope.isEditContact;
-        if($scope.isEditContact) {
-            $scope.contactInfoTmp = angular.copy($scope.contactInfo);
-        } else {
-            $scope.contactInfo = angular.copy($scope.contactInfoTmp);
+    $rootScope.$watch('currentUser.location', function(newValue, oldValue) {
+        if (newValue != null && $rootScope.currentUser.province == null) {
+            let index = getIndexOfArray($scope.provinces, 'id', newValue.parent.id);
+            $rootScope.currentUser.province = $scope.provinces[index];
         }
-    };
+    });
 
-    $scope.isEditContactMean = false;
+    $scope.$watch('currentUser.province', function(newValue, oldValue) {
+        if (newValue == null && $rootScope.currentUser != null) {
+            $rootScope.currentUser.location = null;
+        }
+    });
+
+    $scope.editInfo = function() {
+        $scope.isEditable = !$scope.isEditable;
+
+        if($scope.isEditable) {
+            tempUserInfo = angular.copy($rootScope.currentUser);
+        } else {
+            $rootScope.currentUser = angular.copy(tempUserInfo);
+            delete tempUserInfo;
+        }
+    }
+
+    $scope.isEditableMean = false;
     $scope.editContactMean = function() {
-        $scope.isEditContactMean = !$scope.isEditContactMean;
-        if($scope.isEditContactMean) {
+        $scope.isEditableMean = !$scope.isEditableMean;
+        if($scope.isEditableMean) {
             $scope.contactMeanTmp = angular.copy($scope.contactMean);
         } else {
             $scope.contactMean = angular.copy($scope.contactMeanTmp);
---
+++ gameclub/src/main/java/ec/com/levelap/gameclub/module/game/controller/GameOpenController.java
@@ -0,0 +1,11 @@
+package ec.com.levelap.gameclub.module.game.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="open/game", produces=MediaType.APPLICATION_JSON_VALUE)
+public class GameOpenController {
+	
+}

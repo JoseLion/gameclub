730457c2c9e3f2ed0191f903771d5c75e875e64a
lfgarcia <luis.garcia@provedatos.com>
2017-05-16 12:36:00 -0500

    - Selector de consolas en sección de scroll de juegos.

--- gameclub-web/public/js/modules/core/directives/mostPlayed.js
+++ gameclub-web/public/js/modules/core/directives/mostPlayed.js
@@ -1,27 +1,28 @@
-angular.module('Core').directive('mostPlayed', function($state) {
+angular.module('Core').directive('mostPlayed', function($state, $rootScope) {
 	return {
 		restrict: 'E',
 		templateUrl: 'js/modules/core/directives/mostPlayed.html',
 		scope: {
 			games: '=',
-            previous: '&',
-            next: '&',
+			previous: '&',
+			next: '&',
+			title: '@'
 		},
 		replace: true,
 		link: function($scope, element, attrs, ctrl) {
 
-
-			$scope.$watch('gameConsoles', function(newValue, oldValue) {
+			$scope.consoles = $rootScope.consoles;
+			$scope.$watch('consoles', function(newValue, oldValue) {
 				if(newValue != null) {
 					$scope.gameConsole = {
-						selected : $scope.gameConsoles[0]
+						selected : $scope.consoles[0]
 					};
 				}
 			});
 
 			$scope.viewGameDetail = function(game) {
 				$state.go('gameclub.game', {idGame: 0});
-            };
+			};
 
 		}
 	};

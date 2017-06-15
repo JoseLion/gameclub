d0174e541ca96d725c544c94bd02b246b6b7794a
José Luis León <joseluis.provedatos@gmail.com>
2017-06-15 16:17:24 -0500

    - Avance

--- gameclub-web/beta/js/modules/levelapBlog/resources/articlePreview.js
+++ gameclub-web/beta/js/modules/levelapBlog/resources/articlePreview.js
@@ -1,8 +1,3 @@
-/*!
- * articlePreview.js - v0.1
- * http://www.levelapsoftware.com
- * License: MIT
- */
 angular.module('LevelapBlog').directive('articlePreview', function(openRest, $location, $state) {
 	let baseSrc;
     for (let i = document.getElementsByTagName("script").length - 1; i >= 0; i--) {
@@ -25,21 +20,29 @@ angular.module('LevelapBlog').directive('articlePreview', function(openRest, $lo
 		link: function($scope, element, attrs, ctrl) {
 			$scope.socialShareUrl = $location.$$absUrl;
 			$scope.socialShareUrlDomain = $location.$$protocol + "://" + $location.$$host;
-			if($state.current.name == 'levelapBlog.blog.home') {
+
+			if ($state.current.name == 'levelapBlog.blog.home') {
 				$state.isBlogHome = true;
 			} else {
 				$state.isBlogHome = false;
 			}
+
 			$scope.showAllInfo = false;
 			$scope.currentPage = 0;
 			$scope.showCommentsSection = $scope.comments == null ? false : $scope.comments.content.length > 0;
+			
 			$scope.$on('check-comments', function() {
 				openRest("levelapBlog/getCommentsOf/:articleId/:page").get({articleId: $scope.ngModel.id, page: 0}, function(data) {
                     $scope.comments = data;
                     $scope.showCommentsSection = $scope.comments.content.length > 0;
                 });
 			});
-			if($scope.isComplete != null) {
+
+			$scope.goToDetails = function() {
+				$state.go("levelapBlog.blog.detail", {id: $scope.ngModel.id, title: $scope.ngModel.title});
+			}
+
+			if ($scope.isComplete != null) {
 				if($scope.isComplete == '') {
 					$scope.showAllInfo = true
 				} else {

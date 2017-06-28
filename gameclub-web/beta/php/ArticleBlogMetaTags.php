0e08eac60f578307d56888e6a6316340fd0fd6ca
Gladys Diaz <gladys.diaz@provedatos.com>
2017-06-28 03:55:18 -0500

    facebook

---
+++ gameclub-web/beta/php/ArticleBlogMetaTags.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head >
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:image" content="" />
+	<meta property="fb:app_id"          content="1667722550194317" /> 
+    <!-- etc. -->
+</head>
+ </html>

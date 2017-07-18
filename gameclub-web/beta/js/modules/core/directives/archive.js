2c1e62c8a478039e3f1776cff72f2ce0bb55bdf5
Jose Luis Leon <joseluis.provedatos@gmail.com>
2017-07-17 21:48:36 -0500

    - Mejoras y regreso a directiva archive

--- gameclub-web/beta/js/modules/core/directives/archive.js
+++ gameclub-web/beta/js/modules/core/directives/archive.js
@@ -22,18 +22,15 @@ angular.module("Core").directive('archive', function(openRest, getImageBase64) {
 			function setImageSource() {
 				if ($scope.archive != null) {
 					openRest("archive/downloadFile").download({name: $scope.archive.name, module: $scope.archive.module}, function(data) {
-						let base64 = getImageBase64(data, $scope.archive.type);
-						console.log("base64: ", base64.substring(0, 30));
-
 						if (element[0].tagName == "image") {
-							attrs.$set('xlink:href', base64);
+							attrs.$set('xlink:href', getImageBase64(data, $scope.archive.type));
 						} else {
-							attrs.$set('src', base64);
+							attrs.$set('src', getImageBase64(data, $scope.archive.type));
 							attrs.$set('alt', $scope.archive.name);
 							attrs.$set('title', $scope.archive.title);
 						}
 
-						$scope.ngModel = base64;
+						$scope.ngModel = getImageBase64(data, $scope.archive.type);
 					});
 				}
 			}

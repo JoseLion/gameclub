183aba98a6873223e281b69b694b18131bdc60bf
José Luis León <joseluis.provedatos@gmail.com>
2017-08-02 11:27:53 -0500

    - Avance Sprint 13 - Final detalles Beta

--- gameclub-web/public/js/modules/account/messages/controller/messagesCtrl.js
+++ gameclub-web/public/js/modules/account/messages/controller/messagesCtrl.js
@@ -1,3 +1,22 @@
-angular.module("Messages").controller('MessagesCtrl', function($scope) {
-	
+angular.module("Messages").controller('MessagesCtrl', function($scope, forEach) {
+	$scope.messages = [];
+
+	for (let i = 0; i < 30; i++) {
+		$scope.messages.push({
+			read: i == 0 ? false : true,
+			selected: false
+		});
+	}
+
+	$scope.messageSelected = function(message) {
+		forEach($scope.messages, function(msg) {
+			if (msg.selected) {
+				msg.selected = false;
+				return 'break';
+			}
+		});
+
+		message.read = true;
+		message.selected = true;
+	}
 });

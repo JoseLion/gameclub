6688974d965ae99125f4be8966cafff3f5088ac6
José Luis León <joseluis.provedatos@gmail.com>
2017-08-15 19:44:22 -0500

    - Avance FAQs

--- gameclub-web/public/js/modules/faq/controller/faqsCtrl.js
+++ gameclub-web/public/js/modules/faq/controller/faqsCtrl.js
@@ -1,13 +1,31 @@
-angular.module("Faq").controller('FaqsCtrl', function($scope, categories, openRest) {
+angular.module("Faq").controller('FaqsCtrl', function($scope, categories, openRest, forEach) {
 	categories.$promise.then(function(data) {
 		$scope.categories = data;
 	});
 
 	$scope.categorySelected = function(category) {
-		if (category.faqs == null) {
-			openRest("faq/getFaqsFromCategory/:categoryId", true).get({categoryId: category.id}, function(data) {
-				category.faqs = data;
+		if (!category.selected) {
+			forEach($scope.categories, function(cat) {
+				if (cat.selected) {
+					cat.selected = false;
+					return 'break';
+				}
 			});
+			category.selected = true;
+
+			if (category.faqs == null) {
+				category.isLoading = true;
+
+				openRest("faq/getFaqsFromCategory/:categoryId", true).get({categoryId: category.id}, function(data) {
+					category.faqs = data;
+					$scope.currentCategory = category;
+					category.isLoading = false;
+				}, function(error) {
+					category.isLoading = false;
+				});
+			} else {
+				$scope.currentCategory = category;
+			}
 		}
 	}
 });

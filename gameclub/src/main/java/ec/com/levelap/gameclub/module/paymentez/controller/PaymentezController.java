866daf90f7416ca8473015b713d9900aa3ebdaf6
José Luis León <joseluis.provedatos@gmail.com>
2017-11-14 08:13:53 -0500

    - Pamentez test

--- gameclub/src/main/java/ec/com/levelap/gameclub/module/paymentez/controller/PaymentezController.java
+++ gameclub/src/main/java/ec/com/levelap/gameclub/module/paymentez/controller/PaymentezController.java
@@ -10,15 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import ec.com.levelap.gameclub.module.paymentez.entity.PaymentezResponse;
-
 @RestController
 @RequestMapping(value="open/paymentez", produces=MediaType.APPLICATION_JSON_VALUE)
 public class PaymentezController {
 	
 	@RequestMapping(value="callback", method=RequestMethod.POST)
-	public ResponseEntity<?> callback(@RequestBody PaymentezResponse response) throws ServletException {
-		System.out.println("RESPONSE: " + response.toString());
+	public ResponseEntity<?> callback(@RequestBody Object response) throws ServletException {
+		System.out.println("RESPONSE: " + (String)response);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }

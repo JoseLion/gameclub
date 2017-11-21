9c71e5dbdd1741a4a609d2f57cf4298b995783bc
José Luis León <joseluis.provedatos@gmail.com>
2017-11-21 08:41:48 -0500

    - Fix de kushki en scripts

--- gameclub/src/main/resources/db/gameclub/publicUser/V9_3__PublicUser.sql
+++ gameclub/src/main/resources/db/gameclub/publicUser/V9_3__PublicUser.sql
@@ -9,7 +9,3 @@ INSERT INTO gameclub.public_user(name, last_name, username, password, private_ke
 ('William', 'Huera', 'williamhuera.levelap@gmail.com', '$2a$12$CrGcBjTb/Yszy5LXuVksae5yPHpSI07QQM8ycknRQqIsAQtDXqpba', DECODE('\256wZu\356\2368\347d1\332\340%^', 'escape'), DECODE('\355C\366\360Ij\307\224+ENV\2244M', 'escape')),
 ('Victor', 'Cardenas', 'victorcardenas.levelap@gmail.com', '$2a$12$CrGcBjTb/Yszy5LXuVksae5yPHpSI07QQM8ycknRQqIsAQtDXqpba', DECODE('\256wZu\356\2368\347d1\332\340%^', 'escape'), DECODE('\355C\366\360Ij\307\224+ENV\2244M', 'escape')),
 ('Luisa', 'Trujillo', 'v.cardenas.levelap@outlook.com', '$2a$12$CrGcBjTb/Yszy5LXuVksae5yPHpSI07QQM8ycknRQqIsAQtDXqpba', DECODE('\256wZu\356\2368\347d1\332\340%^', 'escape'), DECODE('\355C\366\360Ij\307\224+ENV\2244M', 'escape'));
-
-INSERT INTO kushki_subscription(card_finale, email, first_name, last_name, status, subscription_id, public_user) VALUES
-('4242','victorcardenas.levelap@gmail.com','victor','cardenas',true,'0000000000008137',(SELECT id FROM gameclub.public_user WHERE username='victorcardenas.levelap@gmail.com')),
-('4242','v.cardenas.levelap@outlook.com','luisa','trujillo',true,'0000000000008137',(SELECT id FROM gameclub.public_user WHERE username='v.cardenas.levelap@outlook.com'));

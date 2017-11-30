b22e14f7e6741f600aa88767d24d515a4e18c945
José Luis León <joseluis.provedatos@gmail.com>
2017-11-30 13:24:57 -0500

    - Reports and address test

--- gameclub-web/admin/js/modules/reports/leaseCosts/controller/leaseCostsCtrl.js
+++ gameclub-web/admin/js/modules/reports/leaseCosts/controller/leaseCostsCtrl.js
@@ -1,10 +1,48 @@
-angular.module('Reports').controller('LeaseCostsCtrl', function($scope, $rootScope, getDTOptions,rest, SweetAlert, $uibModal, $location, $anchorScroll, $state, Const, $http, urlRestPath) {
+angular.module('Reports').controller('LeaseCostsCtrl', function($scope, leaseCosts, getDTOptions, Const, rest, urlRestPath, urlParams) {
+	$scope.search = {};
+	$scope.totalElements;
+	$scope.beginning;
+	$scope.end;
 
-	$scope.dtOptions = getDTOptions.unpaged("fTgitp");
-	$scope.dtColumnDefs = getDTOptions.notSortableAll(3);
-
-	rest("report/leaseCostsPage").get(function(data) {
-		$scope.leaseCostsList = data.content;
+	$scope.dtOptions = getDTOptions.paged().withOption('infoCallback', function(settings, start, end, max, total, pre) {
+		return getDTOptions.infoCallback($scope.totalElements, $scope.beginning, $scope.end);
 	});
 
+	$scope.dtColumnDefs = getDTOptions.notSortableAll(8);
+
+	leaseCosts.$promise.then(function(data) {
+		setPagedData(data);
+	});
+
+	$scope.find = function() {
+		rest("report/leaseCost/find").post($scope.search, function(data) {
+			setPagedData(data);
+		});
+	}
+
+	$scope.clear = function() {
+		$scope.search = {};
+		$scope.find();
+	}
+
+	$scope.pageChanged = function() {
+		$scope.search.page = $scope.currentPage - 1;
+		$scope.find();
+	}
+
+	$scope.getExcelReportUrl = function() {
+		return urlParams(urlRestPath.url + "/api/report/leaseCost/getExcelReport", $scope.search);
+	}
+
+	$scope.getPdfReportUrl = function() {
+		return urlParams(urlRestPath.url + "/api/report/leaseCost/getPdfReport", $scope.search);
+	}
+
+	function setPagedData(data) {
+		$scope.leaseCosts = data.content;
+		$scope.totalElements = data.totalElements;
+		$scope.beginning = (Const.tableSize * data.number) + 1;
+		$scope.end = $scope.beginning + data.numberOfElements - 1;
+		$scope.totalPages = data.totalPages;
+	}
 });

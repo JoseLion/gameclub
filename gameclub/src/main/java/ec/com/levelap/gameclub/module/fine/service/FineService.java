1d07fe05e15f5871dff1477ab9c8406252a0be61
VCardenas <victorcardenas.levelap@gmail.com>
2018-01-23 08:33:42 -0500

    Corrección de bugs

--- gameclub/src/main/java/ec/com/levelap/gameclub/module/fine/service/FineService.java
+++ gameclub/src/main/java/ec/com/levelap/gameclub/module/fine/service/FineService.java
@@ -79,8 +79,9 @@ public class FineService extends BaseService<Fine> {
 				String response = paymentezService.listCurrentUserCards(session);
 				JSONArray jsonArray = new JSONArray(response);
 				paymentezService.debitFromCard(session, request.getRemoteAddr(), jsonArray.getJSONObject(0).getString("card_reference"), totalBalance/*fine.getCardPart()*/, 0.0, "Multa GameClub - " + fine.getDescription());
-				System.out.println("paso tarjeta Multa");
+				
 				publicUser = publicUserService.setUserBalance(publicUser.getId(), 0D);
+				/************************************************************************/
 			} else {
 				fine.setCardPartEnc(null);
 				fine.setBalancePartEnc(fine.getAmountEnc());

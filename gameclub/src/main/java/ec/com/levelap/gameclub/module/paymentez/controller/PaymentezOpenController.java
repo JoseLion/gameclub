888a1c1a8e45e88c8da1cc0447c1a906e52e6bfa
Jose Luis Leon <joseluis.provedatos@gmail.com>
2018-02-07 16:45:56 -0500

    - Callback

--- gameclub/src/main/java/ec/com/levelap/gameclub/module/paymentez/controller/PaymentezOpenController.java
+++ gameclub/src/main/java/ec/com/levelap/gameclub/module/paymentez/controller/PaymentezOpenController.java
@@ -1,28 +1,28 @@
 package ec.com.levelap.gameclub.module.paymentez.controller;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import ec.com.levelap.gameclub.module.fine.entity.Fine;
@@ -50,41 +50,35 @@ public class PaymentezOpenController {
 	private PaymentezService paymentezService;
 	
 	@RequestMapping(value="callback", method=RequestMethod.POST, consumes=MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public ResponseEntity<?> callback(HttpServletRequest servletRequest, Map<String, String> body) throws ServletException, JSONException, IOException, GeneralSecurityException, MessagingException {
-		Set<String> keys = body.keySet();
+	public ResponseEntity<?> callback(HttpServletRequest servletRequest) throws ServletException, JSONException, IOException, GeneralSecurityException, MessagingException, URISyntaxException {
+		String url = "https://gamclub.com.ec?" + IOUtils.toString(servletRequest.getInputStream(), StandardCharsets.UTF_8);
+		List<NameValuePair> params = URLEncodedUtils.parse(new URI(url), StandardCharsets.UTF_8);
+		Map<String, String> response = new HashMap<>();
 		
-		System.out.println("RESPONSE STREAM: " + IOUtils.toString(servletRequest.getInputStream(), StandardCharsets.UTF_8));
-		System.out.println("RESPONSE OBJECT: " + body);
-		for (String key : keys) {
-			System.out.println(key + ": " + body.get(key));
+		for (NameValuePair param : params) {
+			System.out.println(param.getName() + " : " + param.getValue());
+			response.put(param.getName(), param.getValue());
 		}
 		
-		
-		/*JSONObject json = new JSONObject((String) response);
-		String transactioId = json.getString("transaction_id");
-		
+		String transactioId = response.get("transaction_id");
 		Loan loan = loanRepo.findByTransactionId(transactioId);
 		WelcomeKit welcomeKit = welcomeKitRepo.findByTransactionId(transactioId);
 		Fine fine = fineRepo.findByTransactionId(transactioId);
 		
-		if(loan != null) {
-			loan.setAuthCode(json.getString("authorization_code"));
+		if (loan != null) {
+			loan.setAuthCode(response.get("authorization_code"));
 			loan = loanRepo.save(loan);
 			paymentezService.sendMailLoan(loan);
-		} else if(welcomeKit != null) {
-			welcomeKit.setAuthCode(json.getString("authorization_code"));
+		} else if (welcomeKit != null) {
+			welcomeKit.setAuthCode(response.get("authorization_code"));
 			welcomeKit = welcomeKitRepo.save(welcomeKit);
 			paymentezService.sendMailShippinKit(welcomeKit);
-		} else if(fine != null) {
-			fine.setAuthCode(json.getString("authorization_code"));
+		} else if (fine != null) {
+			fine.setAuthCode(response.get("authorization_code"));
 			fine = fineRepo.save(fine);
 			paymentezService.sendMailFine(fine);
-		}*/
+		}
 		
 		return new ResponseEntity<>(HttpStatus.OK);
-	}
-	
-	private static class Callback {
-		public String response;
 	}
 }

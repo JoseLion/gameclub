31fa0b636c6b90b4fc042ae1d57c1afe56adebdf
Jose Luis Leon <joseluis.provedatos@gmail.com>
2018-02-15 10:51:08 -0500

    Sysout paymetez

--- gameclub/src/main/java/ec/com/levelap/gameclub/module/paymentez/service/PaymentezService.java
+++ gameclub/src/main/java/ec/com/levelap/gameclub/module/paymentez/service/PaymentezService.java
@@ -177,6 +177,8 @@ public class PaymentezService {
 				"&auth_timestamp=" + today.getTime() +
 				"&auth_token=" + token;
 		
+		System.out.println("REST URL: " + url);
+		
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity(new URI(url), String.class);
 		return response.getBody();
